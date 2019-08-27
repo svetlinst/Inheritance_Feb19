5d4770d7787b20fc71338a32bd9a866cf287565f
Svetlin Stoyanov <svetlinst@gmail.com>
2019-08-27 23:23:11 +0300

    solutions added

---
+++ BookShop/Book.cs
@@ -0,0 +1,75 @@
+﻿namespace BookShop
+{
+    using System;
+    using System.Text;
+
+    public class Book
+    {
+        private string title;
+        private string author;
+        private decimal price;
+
+        public string Title
+        {
+            get => this.title;
+            protected set
+            {
+                if (value.Length<3)
+                {
+                    throw new ArgumentException("Title not valid!");
+                }
+                this.title = value;
+            }
+        }
+
+        public string Author
+        {
+            get => this.author;
+            protected set
+            {
+                var authorNames = value.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (authorNames.Length > 1)
+                {
+                    if (char.IsDigit(authorNames[1][0]))
+                    {
+                        throw new ArgumentException("Author not valid!");
+                    }
+                }
+                this.author = value;
+            }
+        }
+
+        public virtual decimal Price
+        {
+            get => this.price;
+            protected set
+            {
+                if (value<=0)
+                {
+                    throw new ArgumentException("Price not valid");
+                }
+                this.price = value;
+            }
+        }
+
+        public Book(string author, string title, decimal price)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Price = price;
+        }
+
+        public override string ToString()
+        {
+            var resultBuilder = new StringBuilder();
+            resultBuilder.AppendLine($"Type: {this.GetType().Name}")
+                .AppendLine($"Title: {this.Title}")
+                .AppendLine($"Author: {this.Author}")
+                .AppendLine($"Price: {this.Price:f2}");
+
+            string result = resultBuilder.ToString().TrimEnd();
+            return result;
+
+        }
+    }
+}
